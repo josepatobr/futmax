54cdb1e97c6da3ba73fe886892dcbeb42c22e1b8
jose <jose@DESKTOP-IILTP2S>
2025-01-20 14:11:30 -0300

    nicolas veja se o sistema de login do adm esta lgl

--- dav/views.py
+++ dav/views.py
@@ -2,30 +2,39 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import User
 from django.contrib.auth.decorators import login_required
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse
+
 
 
 def administrador(request:HttpRequest):
     if request.method == "POST":
-        nome_completo = request.POST.get("username")
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
         email = request.POST.get("email")
         password = request.POST.get("password")
-        cpf = request.POST.get("cpf")
         foto_perfil = request.FILES.get("foto_perfil")
-        try:
-            user = User(
-                nome_completo = nome_completo,
+    
+    if User.objects.filter(email=email).exists():
+        messages.error(request, "ja existe um administrador com este email.")
+        return redirect("dav")
+    
+    try:
+        user = User(
+                first_name = first_name,
+                last_name = last_name,
                 email = email,
-                cpf = cpf,
                 foto_perfil = foto_perfil)
-            user.set_password(password)
-            user.save()
-            
-            messages.success(request, "Admin criado com sucesso.")
-            return redirect("home")
-        except Exception as e:
-            messages.error(request, f"Erro ao criar admin: {str(e)}")
-            return render(request, "dav.html")
+        user.set_password(password)
+        user.save()  
+
+        messages.success(request, "Admin criado com sucesso.")
+        return redirect("home")
+        
+    except Exception as e:
+        messages.error(request, f"erro ao criar o admin: {str(e)}")
+        return redirect("dav")
+    
+      
 
 
 
