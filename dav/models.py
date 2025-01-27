2532c5cd8c3fcc0827a789a44b47496e2a697a35
jose <jose@DESKTOP-IILTP2S>
2025-01-27 15:45:00 -0300

    a

--- dav/models.py
+++ dav/models.py
@@ -27,62 +27,9 @@ class User(AbstractUser):
     login_com_email = models.BooleanField(("login com email"), default=False)
     foto_perfil = models.ImageField(upload_to="foto_perfil/", null=True)
     tempo_desativar = 7 # days
-
-    ROLE_ADMIN = "admin"
-    ROLE_USER = "user"
-
-    MAX_NUM_LINKS_PER_USER = 10
-    MAX_NUM_LINKS_TEMP_PER_USER = 10
-
-    ROLE_CHOICES = [
-        (ROLE_ADMIN, ("Admin")),
-        (ROLE_USER, ("User")),
-    ]
-
-    role = models.CharField(("role"),
-        max_length=10,
-        choices=ROLE_CHOICES,
-        default=ROLE_USER,
-        help_text=("User role"))
-    
-    
-    max_num_links = models.PositiveIntegerField(
-        ("max number of links"),
-        default= MAX_NUM_LINKS_PER_USER,
-    )
-    max_num_links_temporary = models.PositiveIntegerField(
-        ("max number of temporary links"),
-        default= MAX_NUM_LINKS_TEMP_PER_USER,
-    )
-
-    @property
-    def is_admin(self) -> bool:
-        return self.role == self.ROLE_ADMIN
-
-    @property
-    def is_user(self) -> bool:
-        return self.role == self.ROLE_USER
     
     def __str__(self):
         return self.username  
-
-    def save(self, *args, **kwargs):
-        role_limits = {
-            self.ROLE_ADMIN: (100, 100),
-            self.ROLE_USER: (
-                self.MAX_NUM_LINKS_PER_USER,
-                self.MAX_NUM_LINKS_TEMP_PER_USER,
-            ),
-        }
-
-        if self.is_superuser:
-            self.role = self.ROLE_ADMIN
-            self.email_verified = True
-
-        if self.role in role_limits:
-            self.max_num_links, self.max_num_links_temporary = role_limits[self.role]
-
-        super().save(*args, **kwargs)
 
 
 class Token(models.Model):
@@ -92,7 +39,7 @@ class Token(models.Model):
     TIPO_LOGAR_EMAIL = "logar_email"
 
     TOKEN_TYPE = [
-        (TIPO_EMAIL_VERIFICADO,("verificar_email")),
+        (TIPO_EMAIL_VERIFICADO,("verificado_email")),
         (TIPO_LOGAR_EMAIL,("logar_email")),
     ]
 
