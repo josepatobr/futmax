b4c8cf24575c9fc3622f5eee5541ed348a5a393c
jose <jose@DESKTOP-IILTP2S>
2025-02-01 14:09:24 -0300

    ta lgl

--- dav/models.py
+++ dav/models.py
@@ -1,25 +1,76 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.timezone import now
 import secrets
+
+
+
+ROLE_DESCONTO_5 = 5
+ROLE_DESCONTO_10 = 10
+ROLE_DESCONTO_15 = 15
+ROLE_DESCONTO_20 = 20
+ROLE_DESCONTO_25 = 25
+
+ROLE_CHOICES = [
+    (5, "5%"),
+    (10, "10%"),
+    (15, "15%"),
+    (20, "20%"),
+    (25, "25%"),
+]
+
 
 
 class Produto(models.Model):
     nome = models.CharField(max_length=500, null=False, blank=False)
-    detalhes = models.TextField(null=False, blank=False)
-    preco = models.CharField(max_length=10, null=False, blank=False)
+    detalhes = models.TextField(null=True, blank=False)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False)
     data_venda = models.DateField()
-    imagem = models.ImageField(upload_to="imagem_produto/", null=False)
+    imagem = models.ImageField(upload_to="imagem_produto/", null=False, blank=False)
 
     def __str__(self):
         return self.nome
 
+    def save(self, *args, **kwargs):
+        if self.preco is not None:
+            self.preco = round(self.preco, 2)
+        
 
-class ProdutoPromocao(Produto):
-    imagem_promocao = models.ImageField(upload_to="imagem_promocao/", null=True, blank=True)
+class ProdutoPromocao(models.Model):
+    nome = models.CharField(max_length=500, null=False, blank=False)
+    especificacoes  = models.TextField(null=True, blank=True)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False)
+    imagem_promocao = models.ImageField(upload_to="imagem_promocao/", null=False, blank=False)
+    limite_maximo = models.DateField(help_text="defina até quando pode ser escolhida uma data de expiração", null=False, blank=False)
+    expiracao_data = models.DateField(help_text="escolha uma data dentro do limite definido", null=False, blank=False)
+    desconto = models.IntegerField(choices=ROLE_CHOICES, default=10)
+
+
+
+    def clean(self):
+        if self.expiracao_data > self.limite_maximo:
+            raise ValidationError({"expiracao_data": "a data de expiração não pode ser maior que o limite maximo escolhido"})
+        if self.expiracao_data < now().date():
+            raise ValidationError({"expiracao_data": "a data de expiração não pode estar no passado"})
+
+    def aplicar_desconto(self, preco):
+        valor_desconto = (preco * self.desconto) / 100
+        preco_final = preco - valor_desconto
+        return preco_final
+        
+    def save(self, *args, **kwargs):
+        self.clean() 
+        super().save(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        if self.preco is not None:
+            self.preco = round(self.preco, 2)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.nome
-
+    
 
 class User(AbstractUser):
     email = models.EmailField("Email address", unique=True, blank=False, null=False)
